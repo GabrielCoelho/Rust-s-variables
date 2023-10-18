ac4c7b4e7288ed957b0ff18267f160db17d6ad1c
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2023-10-18 12:52:42 -0300

    Expressions and Statements

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 fn main() {
-    another_function(5);
-}
+    // cria uma expressão a partir de um novo escopo onde, a variável y terá seu valor atribuido
+    // pelo resultado desta expresão.
+    let y = {
+        // Expresão que irá retornar 4, pois x = 3 e, 3+1 = 4.
+        let x = 3;
+        // note que esta última linha não tem o ponto e vírgula. Pois, se tivesse, seria um
+        // "Statement" e não uma expressão. Esse valor será atribuido para y. Já a linha acima,
+        // possue um ; para declarar que aquilo não será atribuído para o y. 
+        x+1
+    };
 
-fn another_function(x: i32){
-    println!("Outra Função que recebeu o valor: {x}");
+    println!("O valor de y é {y}");
 }
