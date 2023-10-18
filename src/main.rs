4d3109a78bd03f4a109965da4dfce7c5324e0c11
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2023-10-18 13:01:13 -0300

    If statement

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 fn main() {
-    let x = five();
-
-    println!("{x}");
-}
-
-// Por padrão, funções retornam a última linha. 
-// Podemos também retornar algo via `return`
-fn five() -> i32 {
-    5
+    let number = 3;
+    if number < 5 {
+        print!("verdade");
+    }else {
+        print!("mentira");
+    }
 }
