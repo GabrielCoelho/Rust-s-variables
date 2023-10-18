d9b1a9451d1f06de506af515da6f9f048302edc9
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2023-10-18 12:59:23 -0300

    Returning something through fn

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 fn main() {
-    // cria uma expressão a partir de um novo escopo onde, a variável y terá seu valor atribuido
-    // pelo resultado desta expresão.
-    let y = {
-        // Expresão que irá retornar 4, pois x = 3 e, 3+1 = 4.
-        let x = 3;
-        // note que esta última linha não tem o ponto e vírgula. Pois, se tivesse, seria um
-        // "Statement" e não uma expressão. Esse valor será atribuido para y. Já a linha acima,
-        // possue um ; para declarar que aquilo não será atribuído para o y. 
-        x+1
-    };
+    let x = five();
 
-    println!("O valor de y é {y}");
+    println!("{x}");
+}
+
+// Por padrão, funções retornam a última linha. 
+// Podemos também retornar algo via `return`
+fn five() -> i32 {
+    5
 }
