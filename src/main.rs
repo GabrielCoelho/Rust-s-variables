3723e637472e438e9bd6aced3a4f2f76eb685ef6
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2023-10-18 12:43:48 -0300

    Playing with arrays in rust

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
-    // Tuple type
-    let x: (i32, f64, u8) = (500, 6.4, 1);
-    // Assing to a new variable the 0 index of the tuple x
-    let five_hundred = x.0;
-    // Assing to a new variable the 1 index of the tuple x
-    let six_point_four = x.1;
-    // Assing to a new variable the 2 index of the tuple x
-    let one = x.2;
-    println!("the first item in x tuple is: {five_hundred}\nthe second is: {six_point_four}\nand the third is: {one}");
+    //Arrays are declarable through [] brackets. 
+    let a = [1, 2, 3];
+    let mut index = 0;
+    println!("array position {} = {}", index+1, a[index]);
+    index+=1;
+    println!("array position {} = {}", index+1, a[index]);
+    index+=1;
+    println!("array position {} = {}", index+1, a[index]);
+
 }
