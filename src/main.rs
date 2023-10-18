7d62edfbdb7d713ec2878f4149f392e7c54fdd00
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2023-10-18 12:47:19 -0300

    Recieving parameters from main or other fn

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    another_function();
+    another_function(5);
 }
 
-fn another_function(){
-    println!("Outra Função");
+fn another_function(x: i32){
+    println!("Outra Função que recebeu o valor: {x}");
 }
