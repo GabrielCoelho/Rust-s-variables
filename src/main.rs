3f6813bc9a90ba692b2f7af2f73a3e2fc905a872
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2023-10-18 12:23:58 -0300

    The tuple type
    Various types inside one compound type.
    They have fixed length when declared.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 fn main() {
-    // Cria uma variavel do tipo String atribuindo vários espaços à ela. 
-    let spaces = "   ";
-    // Sombrea a primeira  a partir de uma nova variável do tipo Inteiro
-    // isso não funcionaria se na linha 3 fosse criado uma variavel mut, 
-        // pois estaríamos dentro dela tentando mudar o texto com um valor inteiro. 
-    let spaces = spaces.len();
-    println!("o número de espaços contido é de: {spaces}");
+    let tup = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("O valor de y = {y}");
 }
