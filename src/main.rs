34c2cfa98c5f766edaf4ee74bd1820c853006bae
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2023-10-18 12:34:10 -0300

    Adding to new variables the index of the Tuple

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
-    let tup = (500, 6.4, 1);
-    let (x, y, z) = tup;
-    println!("O valor de y = {y}");
+    // Tuple type
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+    // Assing to a new variable the 0 index of the tuple x
+    let five_hundred = x.0;
+    // Assing to a new variable the 1 index of the tuple x
+    let six_point_four = x.1;
+    // Assing to a new variable the 2 index of the tuple x
+    let one = x.2;
+    println!("the first item in x tuple is: {five_hundred}\nthe second is: {six_point_four}\nand the third is: {one}");
 }
