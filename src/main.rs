191085fabc9c2183a01450cb513bb4051a51b7fe
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2023-10-18 12:14:45 -0300

    Explaining the usecase with mutable variables

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ fn main() {
     // Cria uma variavel do tipo String atribuindo vários espaços à ela. 
     let spaces = "   ";
     // Sombrea a primeira  a partir de uma nova variável do tipo Inteiro
+    // isso não funcionaria se na linha 3 fosse criado uma variavel mut, 
+        // pois estaríamos dentro dela tentando mudar o texto com um valor inteiro. 
     let spaces = spaces.len();
     println!("o número de espaços contido é de: {spaces}");
 }
