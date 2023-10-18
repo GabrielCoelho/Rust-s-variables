ccba76f01cfcbbd3edaeb77c225d6d6a65ae5696
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2023-10-18 13:04:49 -0300

    If statement works as same as other languages
    But, you can assing a value to a variable using if inline.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 fn main() {
-    let number = 3;
-    if number < 5 {
-        print!("verdade");
-    }else {
-        print!("mentira");
-    }
+    let condition = true;
+    let number = if condition {5} else {6};
+    println!("{number}");
 }
